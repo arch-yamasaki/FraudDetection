88f232c0d8a82c2ef007bf85ab782db7ba9a6d65
Akira Endo <a_endo@ga-tech.co.jp>
2019-08-11 20:49:03 +0900

    first commit

---
+++ first_detection.py
@@ -0,0 +1,52 @@
+#%%
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import scipy as sp
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from hyperopt import fmin, hp, tpe, Trials, space_eval, STATUS_OK, STATUS_RUNNING
+from functools import partial
+
+import os
+#%%
+# Standard plotly imports
+#import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.tools as tls
+from plotly.offline import iplot, init_notebook_mode
+#import cufflinks
+#import cufflinks as cf
+import plotly.figure_factory as ff
+
+# Using plotly + cufflinks in offline mode
+init_notebook_mode(connected=True)
+#cufflinks.go_offline(connected=True)
+
+# Preprocessing, modelling and evaluating
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix, roc_auc_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score, KFold
+from xgboost import XGBClassifier
+import xgboost as xgb
+
+
+
+#%%
+df_trans = pd.read_csv('../datas/train_transaction.csv')
+df_test_trans = pd.read_csv('../datas/test_transaction.csv')
+
+df_id = pd.read_csv('../datas/train_identity.csv')
+df_test_id = pd.read_csv('../datas/test_identity.csv')
+
+sample_submission = pd.read_csv('../datas/sample_submission.csv', index_col='TransactionID')
+
+df_train = df_trans.merge(df_id, how='left', left_index=True, right_index=True, on='TransactionID')
+df_test = df_test_trans.merge(df_test_id, how='left', left_index=True, right_index=True, on='TransactionID')
+
+print(df_train.shape)
+print(df_test.shape)
+
+# y_train = df_train['isFraud'].copy()
+del df_trans, df_id, df_test_trans, df_test_id
